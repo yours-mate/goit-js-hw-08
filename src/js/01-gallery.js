e2100608d2f5a7df44c1f0f0f976bf40e21d39a0
Provorov Anton <provorovua@gmail.com>
2022-10-24 12:26:18 +0200

    1st task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItem => `
+<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`
+  )
+  .join('');
+
+let modal;
+
+galleryEl.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
