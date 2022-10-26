0539b78f2bf0462ebaf0d12d9ac5bc0fa40b172c
Provorov Anton <provorovua@gmail.com>
2022-10-26 20:13:27 +0200

    goit-js-hw-08 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function timeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+const throttledTimeUpdate = throttle(timeUpdate, 1000);
+
+player.on('timeupdate', throttledTimeUpdate);
+
+localStorage.getItem('videoplayer-current-time')
+  ? player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  : player.setCurrentTime(0);
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const emailEl = document.querySelector('input[name="email"]');
+const messageEL = document.querySelector('textarea[name="message"]');
+
+fillData();
+
+function fillData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    emailEl.value = parsedData.email;
+    messageEL.value = parsedData.message;
+  }
+}
+
+formEl.addEventListener('input', throttle(onFormInputHandle, 500));
+formEl.addEventListener('submit', onFormSubmitHandle);
+
+const data = {
+  email: '',
+  message: '',
+};
+
+function onFormInputHandle(evt) {
+  data[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function onFormSubmitHandle(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
