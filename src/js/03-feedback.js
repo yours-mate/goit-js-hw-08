0af0c0a09e62d531c58583912570f6c09378edf0
Provorov Anton <provorovua@gmail.com>
2022-10-27 11:04:09 +0200

    goit-js-hw-08 redone
    Bug of saved data in task 3 fixed.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,11 +4,15 @@ const formEl = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 const emailEl = document.querySelector('input[name="email"]');
 const messageEL = document.querySelector('textarea[name="message"]');
+const savedData = localStorage.getItem(STORAGE_KEY);
+const data = {
+  email: '',
+  message: '',
+};
 
 fillData();
 
 function fillData() {
-  const savedData = localStorage.getItem(STORAGE_KEY);
   if (savedData) {
     const parsedData = JSON.parse(savedData);
     emailEl.value = parsedData.email;
@@ -19,12 +23,12 @@ function fillData() {
 formEl.addEventListener('input', throttle(onFormInputHandle, 500));
 formEl.addEventListener('submit', onFormSubmitHandle);
 
-const data = {
-  email: '',
-  message: '',
-};
-
 function onFormInputHandle(evt) {
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    data.email = parsedData.email;
+    data.message = parsedData.message;
+  }
   data[evt.target.name] = evt.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
